0d06899cbfcb1624a12cbae4acdda58b338549bb
gao2968 <118335646+gao2968@users.noreply.github.com>
2023-04-13 13:56:26 +0900

    add

--- PadInput.h
+++ PadInput.h
@@ -4,6 +4,9 @@ static XINPUT_STATE OldKey;
 static XINPUT_STATE NowKey;
 static XINPUT_STATE KeyFlg;
 
-void InputController();
-int JudgeButton(unsigned char data);
-int GetStick();
+void InputController();		//コントローラーの入力
+int JudgeButton(unsigned char data);		//ボタンの入力
+int JudgeReleaseButton(unsigned char data);		//ボタンを離したとき
+int GetStickX();		//左スティックのX軸
+int GetStickY();		//左スティックのY軸
+	
