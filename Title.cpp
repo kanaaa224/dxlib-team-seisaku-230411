581941b68d78a3e7d8b58534e4a8a64aefd67f90
nigirimeshi02 <104492636+nigirimeshi02@users.noreply.github.com>
2023-04-13 15:41:56 +0900

    タイトル仮２

--- Title.cpp
+++ Title.cpp
@@ -6,8 +6,19 @@ int SoundTitle;			//タイトルの音楽
 int SoundMoveCorsor;	//カーソル移動の効果音
 int	SoundDecision;		//決定の効果音
 
+int FontHandle;
+
 int LoadImages(void)
 {
+	LPCSTR font_path = "Resources/font/しょかきうたげ（無料版）.ttf"; // 読み込むフォントファイルのパス
+
+	if (AddFontResourceEx(font_path, FR_PRIVATE, NULL) > 0) {
+	}
+	else {
+		// フォント読込エラー処理
+		MessageBox(NULL, "フォント読込失敗", "", MB_OK);
+	}
+
 	if ((Image = LoadGraph("images/TitleImage.png")) == -1)return -1;
 }
 
@@ -18,5 +29,16 @@ int LoadImages(void)
 
 void DrawTitle(void)
 {
-	DrawGraph(0, 0, Image, FALSE);
+	FontHandle= CreateFontToHandle("しょかきうたげ（無料版）", 128,9 , DX_FONTTYPE_EDGE);
+
+	DrawGraph(0, 0, Image, TRUE);
+
+	/*ChangeFont("しょかきうたげ（無料版）");
+	ChangeFontType(DX_FONTTYPE_EDGE);
+	SetFontSize(128);
+	DrawString(340, 140, "りんごおとし", 0x000000, 0xffffff);*/
+
+	DrawStringToHandle(340, 140, "りんごおとし", 0x000000, FontHandle, 0xffffff);
+		
+
 }
