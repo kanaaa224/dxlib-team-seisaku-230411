d20f52411e9ca9af65a42b09e5dcafd9487be287
nigirimeshi02 <104492636+nigirimeshi02@users.noreply.github.com>
2023-04-20 11:15:15 +0900

    ui仮

--- UI.cpp
+++ UI.cpp
@@ -2,15 +2,19 @@
 #include"UI.h"
 #include"main.h"
 #include"init.h"
+#include"resourceLoad.h"
+#include"Apple.h"
 
 extern Init ini;
 extern Game game;
+extern Image image;
 
 const int TIMELIMIT = 60000;		//60ïb
 
 void DrawUserInterFace()
 {
 	DrawTimeLimit();
+	DrawScore();
 }
 
 /*êßå¿éûä‘*/
@@ -21,11 +25,14 @@ void DrawTimeLimit()
 	{
 		game.mode = RESULT;
 	}
-	DrawFormatString(100, 100, 0xff0000, "%d", Time / 1000);
-
+	DrawFormatString(1100, 100, 0xff0000, "%d", Time / 1000);
 }
 
-//void DrawScore()
-//{
-//
-//}
+void DrawScore()
+{
+	//ÉäÉìÉSÇÃï\é¶
+	DrawRotaGraph(1090, 350, 0.125, 0, image.apple[REDAPPLE], TRUE);
+	DrawRotaGraph(1170, 350, 0.125, 0, image.apple[BULEAPPLE], TRUE);
+	DrawRotaGraph(1250, 350, 0.125, 0, image.apple[GOLDAPPLE], TRUE);
+
+}
--- result.cpp
+++ result.cpp
@@ -1,11 +1,13 @@
 #include "DxLib.h"
 #include "resourceLoad.h"
 #include "main.h"
+#include"UI.h"
 
 extern Image image;
 extern Font font;
 
 extern Game game;
+extern UI ui;
 
 /********************************
 * リザルト画面
@@ -14,7 +16,11 @@ void DrawResult() {
 	// 背景表示
 	DrawGraph(0, 0, image.title, TRUE);
 
-	DrawStringToHandle(340, 140, "リザルト画面", 0x000000, font.handle_1_32, 0xffffff);
+	DrawStringToHandle(160, 180, "赤リンゴ", 0x000000, font.handle_1_64, 0xffffff);
+	DrawStringToHandle(160, 250, "青リンゴ", 0x000000, font.handle_1_64, 0xffffff);
+	DrawStringToHandle(160, 320, "黄リンゴ", 0x000000, font.handle_1_64, 0xffffff);
+	DrawStringToHandle(160, 390, "毒リンゴ", 0x000000, font.handle_1_64, 0xffffff);
+	DrawStringToHandle(220, 460, "スコア", 0x000000, font.handle_1_64, 0xffffff);
 
 	// 戻る表示
 	DrawStringToHandle(420, 670, "ESCキーまたはAボタンでもどる", 0x000000, font.handle_1_32, 0xffffff);
