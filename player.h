fae30b525d2761f1af9203e01649516d0a3c508d
gao2968 <118335646+gao2968@users.noreply.github.com>
2023-04-21 13:55:03 +0900

    add anime

--- player.h
+++ player.h
@@ -4,9 +4,9 @@
 #define RUN_RIGHT 25000
 #define WALK_LEFT -2000
 #define RUN_LEFT -25000
-#define MOVE_LEFT_LIMIT 10
+#define MOVE_LEFT_LIMIT 30
 #define MOVE_RIGHT_LIMIT 1000
-#define WALK_SPEED 2
+#define WALK_SPEED 1
 #define SPEED_UP 1
 
 struct PLAYER
@@ -16,6 +16,9 @@ struct PLAYER
 	int y=600;
 	float speed = 0;
 };
+
+static int Run = 0;
+static int Walk = 0;
 
 static struct PLAYER player;
 
