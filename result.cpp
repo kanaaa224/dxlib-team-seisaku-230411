4998b5f0c8d18c9ec3f136636499c3b5d2c8b5e7
moyakino <104491816+moyakino@users.noreply.github.com>
2023-04-24 11:46:11 +0900

    result　終わり

--- result.cpp
+++ result.cpp
@@ -1,8 +1,10 @@
 #include "DxLib.h"
 #include "resourceLoad.h"
+#include <string>
 #include "Apple.h"
 #include "main.h"
 #include "UI.h"
+
 
 extern Image image;
 extern Font font;
@@ -10,11 +12,17 @@ extern Font font;
 extern Game game;
 extern UI ui;
 
+using std::string;
+using std::to_string;
+
 int RED_AppleCount = 0;
 int BLUE_AppleCount = 0;
 int GOLD_AppleCount = 0;
 int POISON_AppleCount = 0;
 int AppleScore = 0;
+
+//int RED_AppleCount = 12345;
+
 
 /********************************
 * リザルト画面
@@ -27,27 +35,30 @@ void DrawResult() { // 未完成
 	POISON_AppleCount	= ReturnPO();
 	AppleScore			= ReturnScore();
 
+	std::string RED = "赤リンゴ：" + std::to_string(RED_AppleCount);
+	std::string BLUE = "青リンゴ：" + std::to_string(BLUE_AppleCount);
+	std::string GOLD = "黄リンゴ：" + std::to_string(GOLD_AppleCount);
+	std::string POISON = "毒リンゴ：" + std::to_string(POISON_AppleCount);
+	std::string SCORE = "スコア：" + std::to_string(AppleScore);
+
 	// 背景表示
 	DrawGraph(0, 0, image.title, TRUE);
 
-	DrawBox(300, 160, 800, 550, 0xffffff, TRUE);
+	DrawBox(300, 160, 800, 550, 0xFDF5E6, TRUE);
+	DrawBox(300, 160, 801, 551, 0x000000, FALSE);
 
 	//赤　青　黄　毒　スコア
-	DrawStringToHandle(320, 180, "赤リンゴ：", 0x000000, font.handle_1_64, 0xffffff);
-	DrawStringToHandle(320, 250, "青リンゴ：", 0x000000, font.handle_1_64, 0xffffff);
-	DrawStringToHandle(320, 320, "黄リンゴ：", 0x000000, font.handle_1_64, 0xffffff);
-	DrawStringToHandle(320, 390, "毒リンゴ：", 0x000000, font.handle_1_64, 0xffffff);
-	DrawStringToHandle(320, 460, "スコア：", 0x000000, font.handle_1_64, 0xffffff);
+	DrawStringToHandle(320, 180, RED.c_str(), 0x000000, font.handle_1_64, 0xffffff);
+	DrawStringToHandle(320, 250, BLUE.c_str(), 0x000000, font.handle_1_64, 0xffffff);
+	DrawStringToHandle(320, 320, GOLD.c_str(), 0x000000, font.handle_1_64, 0xffffff);
+	DrawStringToHandle(320, 390, POISON.c_str(), 0x000000, font.handle_1_64, 0xffffff);
+	DrawStringToHandle(320, 460, SCORE.c_str(), 0x000000, font.handle_1_64, 0xffffff);
 
 	//カウント表示
-	DrawFormatStringFToHandle(660, 180, 0x000000, font.handle_1_64, "%d", RED_AppleCount);
-	DrawFormatStringFToHandle(660, 250, 0x000000, font.handle_1_64, "%d", BLUE_AppleCount);
-	DrawFormatStringFToHandle(660, 320, 0x000000, font.handle_1_64, "%d", GOLD_AppleCount);
-	DrawFormatStringFToHandle(660, 390, 0x000000, font.handle_1_64, "%d", POISON_AppleCount);
-	DrawFormatStringFToHandle(660, 460, 0x000000, font.handle_1_64, "%d", AppleScore);
-
+	//DrawFormatStringFToHandle(660, 180,  0x000000, font.handle_1_64,  "%d", RED_AppleCount);
+	
 	// 戻る表示
-	DrawStringToHandle(530, 670, "Aボタンでもどる", 0x000000, font.handle_1_32, 0xffffff);
+	DrawStringToHandle(530, 670, "Bボタンでもどる", 0x000000, font.handle_1_32, 0xffffff);
 
 	// Space でタイトル
 	if (CheckHitKey(KEY_INPUT_ESCAPE)) {
