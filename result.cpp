c926ce2057b2ebebe7cdb5b0b8e1258886e0d166
moyakino <104491816+moyakino@users.noreply.github.com>
2023-04-24 10:56:41 +0900

    box追加

--- result.cpp
+++ result.cpp
@@ -30,19 +30,21 @@ void DrawResult() {
 	// 背景表示
 	DrawGraph(0, 0, image.title, TRUE);
 
+	DrawBox(300, 160, 800, 550, 0xffffff, TRUE);
+
 	//赤　青　黄　毒　スコア
-	DrawStringToHandle(160, 180, "赤リンゴ：", 0x000000, font.handle_1_64, 0xffffff);
-	DrawStringToHandle(160, 250, "青リンゴ：", 0x000000, font.handle_1_64, 0xffffff);
-	DrawStringToHandle(160, 320, "黄リンゴ：", 0x000000, font.handle_1_64, 0xffffff);
-	DrawStringToHandle(160, 390, "毒リンゴ：", 0x000000, font.handle_1_64, 0xffffff);
-	DrawStringToHandle(220, 460, "スコア：", 0x000000, font.handle_1_64, 0xffffff);
+	DrawStringToHandle(320, 180, "赤リンゴ：", 0x000000, font.handle_1_64, 0xffffff);
+	DrawStringToHandle(320, 250, "青リンゴ：", 0x000000, font.handle_1_64, 0xffffff);
+	DrawStringToHandle(320, 320, "黄リンゴ：", 0x000000, font.handle_1_64, 0xffffff);
+	DrawStringToHandle(320, 390, "毒リンゴ：", 0x000000, font.handle_1_64, 0xffffff);
+	DrawStringToHandle(320, 460, "スコア：", 0x000000, font.handle_1_64, 0xffffff);
 
 	//カウント表示
-	DrawFormatStringFToHandle(500, 180, 0x000000, font.handle_1_64, "%d", RED_AppleCount);
-	DrawFormatStringFToHandle(500, 250, 0x000000, font.handle_1_64, "%d", BLUE_AppleCount);
-	DrawFormatStringFToHandle(500, 320, 0x000000, font.handle_1_64, "%d", GOLD_AppleCount);
-	DrawFormatStringFToHandle(500, 390, 0x000000, font.handle_1_64, "%d", POISON_AppleCount);
-	DrawFormatStringFToHandle(500, 460, 0x000000, font.handle_1_64, "%d", AppleScore);
+	DrawFormatStringFToHandle(660, 180, 0x000000, font.handle_1_64, "%d", RED_AppleCount);
+	DrawFormatStringFToHandle(660, 250, 0x000000, font.handle_1_64, "%d", BLUE_AppleCount);
+	DrawFormatStringFToHandle(660, 320, 0x000000, font.handle_1_64, "%d", GOLD_AppleCount);
+	DrawFormatStringFToHandle(660, 390, 0x000000, font.handle_1_64, "%d", POISON_AppleCount);
+	DrawFormatStringFToHandle(660, 460, 0x000000, font.handle_1_64, "%d", AppleScore);
 
 	// 戻る表示
 	DrawStringToHandle(420, 670, "ESCキーまたはAボタンでもどる", 0x000000, font.handle_1_32, 0xffffff);
