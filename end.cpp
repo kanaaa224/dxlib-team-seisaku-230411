538676eb33f6d1f6b0ed76ab82e1db2a0cf8719b
issei-toyomoto <120348418+issei-toyomoto@users.noreply.github.com>
2023-05-08 14:15:53 +0900

    クレジット

--- end.cpp
+++ end.cpp
@@ -22,14 +22,18 @@ void DrawEnd() { // 未完成
 		// Thank you for playing
 		DrawStringToHandle(10, 10, "Thank you for playing", 0x000000, font.handle_1_128, 0xffffff);
 
+		//製作者
+		DrawStringToHandle(10, 150, "製作者", 0x000000, font.handle_1_64, 0xffffff);
+		DrawStringToHandle(10, 230, "新里滉規　北村隼士 島袋 叶望　玉城流叶　豊元一成",0x000000, font.handle_1_32, 0xffffff);
+
 		// 謝辞
 		//SetFontSize(40); FPS低下問題発生中
 		//DrawFormatString(20, 200, 0x000000, "使用した素材のクレジット");
 		//SetFontSize(32);
-		DrawStringToHandle(10, 200, "使用した素材のクレジット", 0x000000, font.handle_1_64, 0xffffff);
-		DrawStringToHandle(10, 280, "プレイヤー画像：AAA様", 0x000000, font.handle_1_32, 0xffffff);
-		DrawStringToHandle(10, 320, "リンゴ画像：AAA様", 0x000000, font.handle_1_32, 0xffffff);
-		DrawStringToHandle(10, 360, "フォント：AAA様", 0x000000, font.handle_1_32, 0xffffff);
+		DrawStringToHandle(10, 270, "使用した素材", 0x000000, font.handle_1_64, 0xffffff);
+		DrawStringToHandle(10, 350, "プレイヤー画像：AAA様", 0x000000, font.handle_1_32, 0xffffff);
+		DrawStringToHandle(10, 390, "リンゴ画像：AAA様", 0x000000, font.handle_1_32, 0xffffff);
+		DrawStringToHandle(10, 430, "フォント：AAA様", 0x000000, font.handle_1_32, 0xffffff);
 
 		if (CheckHitKey(KEY_INPUT_ESCAPE)) {
 			game.mode = EXIT;
