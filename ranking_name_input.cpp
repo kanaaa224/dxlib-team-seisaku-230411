8740c665b73ec7b014556909ac753e8d6f6ae44b
kanaaa224 <kanaaa224@outlook.com>
2023-05-09 12:56:39 +0900

    Update ranking_name_input.cpp

--- ranking_name_input.cpp
+++ ranking_name_input.cpp
@@ -338,9 +338,9 @@ void DrawRankingNameInput() { // 島袋が担当中、入力された名前を返す関数と、SetFo
 * 入力された名前を返す
 ********************************/
 char GetInputedName(int i) {
-	char name[10];
+	/*char name[10];
 	char name1[10] = "AED";
 	strcpy(name,name1);
-	return name[i];
-	//return 'aaaa';
+	return name[i];*/
+	return 'aaaa';
 };
