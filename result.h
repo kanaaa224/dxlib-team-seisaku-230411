5c02e98b030f66cdabbf6cbb06c4fbbe24bf798a
moyakino <104491816+moyakino@users.noreply.github.com>
2023-05-10 12:05:08 +0900

    コメント

--- result.h
+++ result.h
@@ -1,9 +1,9 @@
 #pragma once
 
-extern int RED_AppleCount;
-extern int BLUE_AppleCount;
-extern int GOLD_AppleCount;
-extern int POISON_AppleCount;
-extern int AppleScore;
+extern int RED_AppleCount;		//赤リンゴ　個数用
+extern int BLUE_AppleCount;		//青リンゴ　個数用
+extern int GOLD_AppleCount;		//金リンゴ　個数用
+extern int POISON_AppleCount;	//毒リンゴ　個数用
+extern int AppleScore;			//スコア　　個数用
 
 void DrawResult();
