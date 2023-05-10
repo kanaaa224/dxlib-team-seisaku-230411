6ac77e05117228cef5257dd5596d09500e45fbf7
moyakino <104491816+moyakino@users.noreply.github.com>
2023-05-10 12:01:56 +0900

    コメント　追加

--- result.cpp
+++ result.cpp
@@ -6,8 +6,9 @@
 #include"UI.h"
 #include "ranking.h"
 #include "PadInput.h"
+#include "result.h"
 
-extern Image image;
+extern Image image;	
 extern Font font;
 
 extern Game game;
@@ -16,14 +17,11 @@ extern UI ui;
 using std::string;
 using std::to_string;
 
-int RED_AppleCount = 0;
-int BLUE_AppleCount = 0;
-int GOLD_AppleCount = 0;
-int POISON_AppleCount = 0;
-int AppleScore = 0;
-
-//int RED_AppleCount = 12345;
-
+extern int RED_AppleCount		= 0;	//赤リンゴ　個数用
+extern int BLUE_AppleCount		= 0;	//青リンゴ　個数用
+extern int GOLD_AppleCount		= 0;	//金リンゴ　個数用
+extern int POISON_AppleCount	= 0;	//毒リンゴ　個数用
+extern int AppleScore			= 0;	//スコア　　個数用
 
 /********************************
 * リザルト画面
@@ -31,27 +29,31 @@ int AppleScore = 0;
 
 void DrawResult() { // 未完成
 
-	RED_AppleCount		= ReturnRA();
-	BLUE_AppleCount		= ReturnBL();
-	GOLD_AppleCount		= ReturnGL();
-	POISON_AppleCount	= ReturnPO();
-	AppleScore			= ReturnScore();
+	//個数及びスコアの受け渡し
+	RED_AppleCount		= ReturnRA();		//returnで返した赤リンゴの個数を代入
+	BLUE_AppleCount		= ReturnBL();		//returnで返した青リンゴの個数を代入
+	GOLD_AppleCount		= ReturnGL();		//returnで返した金リンゴの個数を代入
+	POISON_AppleCount	= ReturnPO();		//returnで返した毒リンゴの個数を代入
+	AppleScore			= ReturnScore();	//returnで返したスコアを代入
 
-	std::string RED = "赤リンゴ：" + std::to_string(RED_AppleCount);
-	std::string BLUE = "青リンゴ：" + std::to_string(BLUE_AppleCount);
-	std::string GOLD = "黄リンゴ：" + std::to_string(GOLD_AppleCount);
-	std::string POISON = "毒リンゴ：" + std::to_string(POISON_AppleCount);
-	std::string SCORE = "スコア：" + std::to_string(AppleScore);
+	//文字列変換	個数を増分する表示はフォントが適応されないため文字列に変換
+	std::string RED = "赤リンゴ：" + std::to_string(RED_AppleCount);		//REDに赤リンゴ：個数の文字列を作成　
+	std::string BLUE = "青リンゴ：" + std::to_string(BLUE_AppleCount);		//BLUEに青リンゴ：個数の文字列を作成
+	std::string GOLD = "黄リンゴ：" + std::to_string(GOLD_AppleCount);		//GOLDに金リンゴ：個数の文字列を作成
+	std::string POISON = "毒リンゴ：" + std::to_string(POISON_AppleCount); //POISONに毒リンゴ：個数の文字列を作成
+	std::string SCORE = "スコア：" + std::to_string(AppleScore);			//SCOREにスコア：スコアの文字列を作成
 
 	// 背景表示
 	DrawGraph(0, 0, image.title, TRUE);
 
+	//リザルト
 	DrawStringToHandle(320, 70, "RESULT", 0x000000, font.handle_1_64, 0xffffff);
 
+	//背景　白
 	DrawBox(300, 160, 800, 550, 0xFDF5E6, TRUE);
 	DrawBox(300, 160, 801, 551, 0x000000, FALSE);
 
-	//赤　青　黄　毒　スコア
+	//赤　青　黄　毒　スコア の文字列(String)を文字(Char)に変換
 	DrawStringToHandle(320, 180, RED.c_str(), 0x000000, font.handle_1_64, 0xffffff);
 	DrawStringToHandle(320, 250, BLUE.c_str(), 0x000000, font.handle_1_64, 0xffffff);
 	DrawStringToHandle(320, 320, GOLD.c_str(), 0x000000, font.handle_1_64, 0xffffff);
--- result.h
+++ result.h
@@ -1,3 +1,9 @@
 #pragma once
 
+extern int RED_AppleCount;
+extern int BLUE_AppleCount;
+extern int GOLD_AppleCount;
+extern int POISON_AppleCount;
+extern int AppleScore;
+
 void DrawResult();
