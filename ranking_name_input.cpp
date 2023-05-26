84ee21f713f524bb1745e26ad3780d1f5d8d80a4
gao2968 <118335646+gao2968@users.noreply.github.com>
2023-05-26 14:05:57 +0900

    a

--- ranking_name_input.cpp
+++ ranking_name_input.cpp
@@ -22,7 +22,6 @@ char RankingNameInput::alphabet[][26] = {
    { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' },
    { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' }
 };
-//const char rni_qwerty[] = { 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'z', 'x', 'c', 'v', 'b', 'n', 'm', 'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', 'Z', 'X', 'C', 'V', 'B', 'N', 'M' };
 int RankingNameInput::ctrl_state[] = {0, 0, 0, 0};
 
 
